6324159bc704d1916a8cad2850059c631a617651
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-25 10:55:17 +0200

    creazione progetto

---
+++ src/main/java/andrearuzittu/EpicodeItjava6D5/EpicodeItJava6D5Application.java
@@ -0,0 +1,13 @@
+package andrearuzittu.EpicodeItjava6D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeItJava6D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeItJava6D5Application.class, args);
+	}
+
+}
