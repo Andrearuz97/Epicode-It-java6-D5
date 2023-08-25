e2851c7f28c4ded85b5e376eb5d935c9a1cc1de2
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-25 12:34:38 +0200

    implementazione fireProb(provvisorio)

--- src/main/java/andrearuzittu/EpicodeItjava6D5/entities/FireProbe.java
+++ src/main/java/andrearuzittu/EpicodeItjava6D5/entities/FireProbe.java
@@ -1,5 +1,41 @@
 package andrearuzittu.EpicodeItjava6D5.entities;
 
-public class FireProbe {
+import andrearuzittu.EpicodeItjava6D5.interfaces.Observer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
+public class FireProbe implements Observer {
+
+	private Long id;
+	private String latitudine;
+	private String longitudine;
+	private Integer smokeLevel = 0;
+	private ControlCenter controlCenter;
+
+	public FireProbe(Long id, String latitudine, String longitudine, Integer smokeLevel) {
+
+		this.id = id;
+		this.latitudine = latitudine;
+		this.longitudine = longitudine;
+		this.smokeLevel = smokeLevel;
+	}
+
+	@Override
+	public void smokeLevelControl() {
+
+		if (this.smokeLevel >= 5) {
+			String notificationUrl = generateNotificationUrl();
+			System.out.println("Attention! the probe n. " + id + " has registered a level of smoke " + smokeLevel
+					+ ", latitudine: " + latitudine + " longitudine: " + longitudine);
+			System.out.println("\"The control center sent the notification URL: " + notificationUrl);
+		}
+
+	}
+
+	public String generateNotificationUrl() {
+		return "http://host/alarm?idsonda=" + id + "&lat=" + latitudine + "&lon=" + longitudine + "&smokelevel="
+				+ smokeLevel;
+	}
 }
