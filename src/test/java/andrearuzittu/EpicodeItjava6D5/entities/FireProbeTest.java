b82006f24ddd2b6619d08b75189c87a1a54f4c0e
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-25 13:52:16 +0200

    Update FireProbeTest.java

--- src/test/java/andrearuzittu/EpicodeItjava6D5/entities/FireProbeTest.java
+++ src/test/java/andrearuzittu/EpicodeItjava6D5/entities/FireProbeTest.java
@@ -12,4 +12,20 @@ public class FireProbeTest {
 		assertEquals(0, probe.getSmokeLevel());
 	}
 
+	@Test
+	public void testSetSmokeLevel() {
+		FireProbe probe = new FireProbe(1L, "41.9028", "12.4964", 0);
+		probe.setSmokeLevel(5);
+		assertEquals(5, probe.getSmokeLevel());
+	}
+
+	@Test
+	public void testGenerateNotificationUrl() {
+		FireProbe probe = new FireProbe(1L, "41.9028", "12.4964", 0);
+		probe.setSmokeLevel(3);
+		String expectedUrl = "http://host/alarm?idsonda=1&lat=41.9028&lon=12.4964&smokelevel=3";
+		assertEquals(expectedUrl, probe.generateNotificationUrl());
+	}
+
+
 }
