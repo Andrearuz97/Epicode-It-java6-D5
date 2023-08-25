f92a8a4d2e18ad5638341aa40280dcb0f4d3aa3e
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-25 14:18:30 +0200

    Update ControlCenterTest.java

--- src/test/java/andrearuzittu/EpicodeItjava6D5/entities/ControlCenterTest.java
+++ src/test/java/andrearuzittu/EpicodeItjava6D5/entities/ControlCenterTest.java
@@ -1,5 +1,53 @@
 package andrearuzittu.EpicodeItjava6D5.entities;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class ControlCenterTest {
 
+	@Test
+	public void testRegisterObserver() {
+		ControlCenter cc = new ControlCenter();
+		FireProbe probe = new FireProbe(1L, "41.9028", "12.4964", 0);
+		cc.register(probe);
+		assertTrue(cc.getProbesList().contains(probe));
+	}
+
+	@Test
+	public void testUnregisterObserver() {
+		ControlCenter cc = new ControlCenter();
+		FireProbe probe = new FireProbe(1L, "41.9028", "12.4964", 0);
+		cc.register(probe);
+		cc.unregister(probe);
+		assertFalse(cc.getProbesList().contains(probe));
+	}
+
+	@Test
+	public void testNotifyObserversAboveThreshold() {
+		ControlCenter cc = new ControlCenter();
+		FireProbe probe1 = new FireProbe(1L, "41.9028", "12.4964", 0);
+		FireProbe probe2 = new FireProbe(2L, "34.0522", "-118.2437", 0);
+		cc.register(probe1);
+		cc.register(probe2);
+
+		probe1.setSmokeLevel(6);
+		cc.notifyObservers();
+
+	}
+
+	@Test
+	public void testNotifyObserversBelowThreshold() {
+		ControlCenter cc = new ControlCenter();
+		FireProbe probe1 = new FireProbe(1L, "41.9028", "12.4964", 0);
+		FireProbe probe2 = new FireProbe(2L, "34.0522", "-118.2437", 0);
+		cc.register(probe1);
+		cc.register(probe2);
+
+		probe1.setSmokeLevel(3);
+		cc.notifyObservers();
+
+	}
 }
+
