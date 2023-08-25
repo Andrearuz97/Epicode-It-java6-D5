d0dbb2e6dbbd5892482c2e4e82f1531ad6883a58
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-25 12:06:32 +0200

    implementazione(provvisoria) centro di controllo

--- src/main/java/andrearuzittu/EpicodeItjava6D5/entities/ControlCenter.java
+++ src/main/java/andrearuzittu/EpicodeItjava6D5/entities/ControlCenter.java
@@ -1,5 +1,37 @@
 package andrearuzittu.EpicodeItjava6D5.entities;
 
-public class ControlCenter {
+import java.util.ArrayList;
+import java.util.List;
 
+import andrearuzittu.EpicodeItjava6D5.interfaces.Observer;
+import andrearuzittu.EpicodeItjava6D5.interfaces.Subject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ControlCenter implements Subject {
+
+	private Long id;
+
+	private List<FireProbe> probesList = new ArrayList<>();
+
+	@Override
+	public void register(Observer o) {
+		probesList.add((FireProbe) o);
+	}
+
+	@Override
+	public void unregister(Observer o) {
+		probesList.remove(o);
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (FireProbe p : probesList) {
+			p.smokeLevelControl();
+		}
+	}
 }
